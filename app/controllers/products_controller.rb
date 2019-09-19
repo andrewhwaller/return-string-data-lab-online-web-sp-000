c4609a5977a45b63689db5af0c36cb5f405fa084
Andrew Waller <andrewhwaller@gmail.com>
2019-09-19 06:44:09 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,6 +31,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:title, :description)
+    params.require(:product).permit(:name, :price, :inventory, :description)
   end
 end
